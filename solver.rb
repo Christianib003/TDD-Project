a344d59d59e3d0fd01b590ece4a9de8b3f97a4b3
David-Kasilia <davidkasilia001@gmail.com>
2022-11-10 15:54:39 +0300

    Created Factorial method and run rspec test

--- solver.rb
+++ solver.rb
@@ -1,1 +1,10 @@
-
+class Solver
+    def factorial(n)
+        if n == 0
+            return 1
+        elsif n < 0
+            raise 'Exception Created: Factorial ERROR Number is Negative'
+        end
+        n * factorial(n - 1)
+    end
+end
