e7d02dfb13b3f861aa022204e97b7da9e97ce2af
Christianib003 <christianib003@gmail.com>
2022-11-10 15:19:54 +0200

    add fizzbuzz method

--- solver.rb
+++ solver.rb
@@ -1,10 +1,26 @@
 class Solver
-    def factorial(n)
-        if n == 0
-            return 1
-        elsif n < 0
-            raise 'Exception Created: Factorial ERROR Number is Negative'
-        end
-        n * factorial(n - 1)
+  def factorial(num)
+    if num.zero?
+      1
+    elsif num.negative?
+      raise 'Exception Created: Factorial ERROR Number is Negative'
+    else
+      num * factorial(num - 1)
     end
+  end
+
+  def fizzbuzz(num)
+    fizz = (num % 3).zero?
+    buzz = (num % 5).zero?
+    fizz_buzz = (num % 3).zero? && (num % 5).zero?
+    if fizz_buzz
+      'fizzbuzz'
+    elsif fizz
+      'fizz'
+    elsif buzz
+      'buzz'
+    else
+      num.to_s
+    end
+  end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,38 +2,38 @@ require_relative '../solver'
 
 describe 'TDD unit tests fot Solver class methods' do
   before(:each) do
-      @solution = Solver.new
+    @solution = Solver.new
   end
   # Factorial Method unit tests
   context 'Unit test for factorial method' do
-      it 'should return a factorial value for a positive integer' do
-          factorial = @solution.factorial(8)
-          expect(factorial).to eq(40320)
-      end
-      it 'should return 1 if the value of n is zero' do
-          factorial = @solution.factorial(0)
-          expect(factorial).to eq(1)
-      end
-      it 'should raise a Runtime error if the value of n is a negative value' do
-          expect { Solver.new.factorial(-1) }.to raise_error(RuntimeError)
-      end
+    it 'should return a factorial value for a positive integer' do
+      factorial = @solution.factorial(8)
+      expect(factorial).to eq(40_320)
+    end
+    it 'should return 1 if the value of n is zero' do
+      factorial = @solution.factorial(0)
+      expect(factorial).to eq(1)
+    end
+    it 'should raise a Runtime error if the value of n is a negative value' do
+      expect { Solver.new.factorial(-1) }.to raise_error(RuntimeError)
+    end
   end
   context 'test fizzbuzz method' do
     it 'should return fizzbuzzfor numbers divisible by both 5 and 3' do
-        number = @solver.fizzbuzz(15)
-        expect(number).to eq('fizzbuzz')
+      number = @solution.fizzbuzz(15)
+      expect(number).to eq('fizzbuzz')
     end
     it 'should return fizz for numbers divisible by 3 only' do
-        number = @solver.fizzbuzz(9)
-        expect(number).to eq('fizz')
+      number = @solution.fizzbuzz(9)
+      expect(number).to eq('fizz')
     end
     it 'should return buzz for numbers which are divisible by 5 only' do
-        number = @solver.fizzbuzz(25)
-        expect(number).to eq('buzz')
+      number = @solution.fizzbuzz(25)
+      expect(number).to eq('buzz')
     end
     it 'should return the input number as a string if it is neither divisible by 3 nor 5' do
-      number = @solver.fizzbuzz(2)
+      number = @solution.fizzbuzz(2)
       expect(number).to eq('2')
     end
-  end  
+  end
 end
